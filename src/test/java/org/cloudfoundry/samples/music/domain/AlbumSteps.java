5cc10ede316665d31f8c2e343bf7d6395a525007
Sergiu <sbodiu@pivotal.io>
2016-05-13 00:08:11 +0800

    add more test scenarios

--- src/test/java/org/cloudfoundry/samples/music/domain/AlbumSteps.java
+++ src/test/java/org/cloudfoundry/samples/music/domain/AlbumSteps.java
@@ -29,4 +29,34 @@ public class AlbumSteps {
         throw new PendingException();
     }
 
+    @When("^I search for Album Brothers$")
+    public void i_search_for_Album_Brothers() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I add Album Brothers$")
+    public void i_add_Album_Brothers() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Album Brothers can be found$")
+    public void album_Brothers_can_be_found() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I update Album year$")
+    public void i_update_Album_year() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Album Brothers was updated$")
+    public void album_Brothers_was_updated() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
 }
