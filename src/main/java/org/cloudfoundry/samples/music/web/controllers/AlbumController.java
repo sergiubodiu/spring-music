980d49a78747c0babcdb3fce489e06b3d515954b
Sergiu <sbodiu@pivotal.io>
2016-05-12 23:48:37 +0800

    rest controller enhancements

--- src/main/java/org/cloudfoundry/samples/music/web/controllers/AlbumController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/controllers/AlbumController.java
@@ -5,12 +5,11 @@ import org.cloudfoundry.samples.music.domain.AlbumRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping(value = "/albums")
 public class AlbumController {
     private static final Logger logger = LoggerFactory.getLogger(AlbumController.class);
@@ -21,27 +20,23 @@ public class AlbumController {
         this.repository = repository;
     }
 
-    @ResponseBody
     @GetMapping
     public Iterable<Album> albums() {
         return repository.findAll();
     }
 
-    @ResponseBody
     @PutMapping
     public Album add(@RequestBody @Valid Album album) {
         logger.info("Adding album " + album.getId());
         return repository.save(album);
     }
 
-    @ResponseBody
     @PostMapping
     public Album update(@RequestBody @Valid Album album) {
         logger.info("Updating album " + album.getId());
         return repository.save(album);
     }
 
-    @ResponseBody
     @GetMapping("/{id}")
     public Album getById(@PathVariable String id) {
         logger.info("Getting album " + id);
